4af2f9878b5902753725efbb07f3eba487fa1a2f
Auricel <148658628+Auricel@users.noreply.github.com>
2024-10-06 12:36:38 +0000

    Listo

--- src/models.py
+++ src/models.py
@@ -7,26 +7,45 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(20), nullable=False, unique=True)
+    firstname = Column(String(20), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    password = Column(String(50), nullable=False)
+    username = Column(String(20), nullable=False, unique=True)
+    suscriptionDate = Column(String(50), nullable=False)
+    
+    favorites = relationship('Favorites', backref='user')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(30), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
 
-## Draw from SQLAlchemy base
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
+
+    usuario = relationship('Usuario', backref='favorites') 
+    characters = relationship('Characters', backref='favorites')
+    planets = relationship('Planets', backref='favorites')
+    vehicles = relationship('Vehicles', backref='favorites')
+
+# Generar el diagrama
 render_er(Base, 'diagram.png')
